eb65c90a7623b1340bb0710af7a4291734b6b798
WXQ <i-vrstudio@foxmail.com>
2018-05-23 22:42:47 +0800

    Revise loading icon and coding formatting

--- WX News Miniprogram/pages/index/index.js
+++ WX News Miniprogram/pages/index/index.js
@@ -3,111 +3,120 @@ const app = getApp()
 // 导入时间格式转换工具
 const util = require('../../utils/util.js')
 Page({
-  data: {
-    //新闻分类标题数组, wx:for-item = nlType = {key:, name:}
-      newsList:  [  { key:'gn', name: '国内'
-                     }, { key:'gj',  name: '国际'
-                     }, { key:'cj',  name: '财经'
-                     }, { key: 'yl', name: '娱乐'
-                     }, { key: 'js', name: '军事'
-                     }, { key: 'ty', name: '体育'
-                     }, { key: 'other', name: '其他'
-                     }],
-      activeType: 'gn',
-      newsResults: [], //保存新闻数据组
-   } ,
-  //启动
-  onLoad() {
-     this.getNews()
-  },
-  //下拉刷新，更新数据
-  onPullDownRefresh() {
-     this.getNews(() => {
-        wx.stopPullDownRefresh()
-     })
-  },
-  //获取API数据，更新数据
-  getNews(callback) {
-     const nltype = this.data.activeType
-      //console.log(nltype+'  get news nltype for api input')
-     wx.request({
-        url: 'https://test-miniprogram.com/api/news/list',
-        data: {
-           type: nltype
-        },
-        //数据数组成功返回
-        success: res => {
-            let results = res.data.result
-            let newsLength = results.length //length of result data array
-            let code =res.data.code
-            let message = res.data.message
-            let newsResults = [] //api news result array
-           // console.log(results, code, message) 
-            //testing api results
-            if (code=="200" && message=="success") {
-               for (let i = 0; i < newsLength; i += 1) {
-                  newsResults.push({
-                     newsTitle: results[i].title,
-                     newsDate: util.formatTime(new Date(results[i].date)),
-                     newsSource: results[i].source,
-                     //imagePath: results[i].firstImage,
-                     imagePath: !!results[i].firstImage ? results[i].firstImage : '/images/image-icon.png',  
-                                       //检查是否有配图
-                     id: results[i].id
-                  })
-               }
-               this.setData({
-                  newsResults: newsResults, //update data
-               })
-            //  console.log(newsResults)  //testing setData result
-            } else {
-                  wx.showToast({
-                     title: '加载错误, 请重试',
-                     icon: 'none',
-                     duration: 1800
-                  })
+    data: {
+        //新闻分类标题数组, wx:for-item = nlType = {key:, name:}
+        newsList: [{
+            key: 'gn', name: '国内'
+        }, {
+            key: 'gj', name: '国际'
+        }, {
+            key: 'cj', name: '财经'
+        }, {
+            key: 'yl', name: '娱乐'
+        }, {
+            key: 'js', name: '军事'
+        }, {
+            key: 'ty', name: '体育'
+        }, {
+            key: 'other', name: '其他'
+        }],
+        activeType: 'gn',
+        newsResults: [], //保存新闻数据组
+    },
+    //启动
+    onLoad() {
+        this.getNews()
+    },
+    //下拉刷新，更新数据
+    onPullDownRefresh() {
+        this.getNews(() => {
+            wx.stopPullDownRefresh()
+        })
+    },
+    //获取API数据，更新数据
+    getNews(callback) {
+        const nltype = this.data.activeType
+        //console.log(nltype+'  get news nltype for api input')
+        wx.request({
+            url: 'https://test-miniprogram.com/api/news/list',
+            data: {
+                type: nltype
+            },
+            //数据数组成功返回
+            success: res => {
+                let results = res.data.result
+                let newsLength = results.length //length of result data array
+                let code = res.data.code
+                let message = res.data.message
+                let newsResults = [] //api news result array
+                // console.log(results, code, message) 
+                //testing api results
+                if (code == "200" && message == "success") {
+                    for (let i = 0; i < newsLength; i += 1) {
+                        newsResults.push({
+                            newsTitle: results[i].title,
+                            newsDate: util.formatTime(new Date(results[i].date)),
+                            newsSource: results[i].source,
+                            //imagePath: results[i].firstImage,
+                            imagePath: !!results[i].firstImage ? results[i].firstImage : '/images/image-icon.png',
+                            //检查是否有配图
+                            id: results[i].id
+                        })
+                    }
+                    this.setData({
+                        newsResults: newsResults, //update data
+                    })
+                    //  console.log(newsResults)  //testing setData result
+                    wx.hideLoading({
+                        title: '加载中',
+                    })
+                } else {
+                    wx.showToast({
+                        title: '加载错误, 请重试',
+                        icon: 'none',
+                        duration: 1800
+                    })
+                }
+            },
+            fall: () => {
+                wx.showToast({
+                    title: '加载错误,请重试',
+                    icon: 'none',
+                    duration: 1800
+                })
+            },
+            complete: () => {
+                callback && callback()
             }
-        },
-        fall: () => {
-           wx.showToast({
-              title: '加载错误,请重试',
-              icon: 'none',
-              duration: 1800
-           })
-        },
-        complete: () => {
-           callback && callback()
-        }
-     })
-  },
+        })
+    },
 
-  //激活当前新闻类别，更新获取新闻数据，显示loading
-  switchNewsType(event) {
-      const newType = event.currentTarget.dataset.nltype
-     // console.log(newType + ' new news type after click') //check news type key
-      if (newType !== this.data.activeType) {
-               this.setData({
-                     activeType: newType
-               })
-               this.getNews()
-              // console.log(newType + ' after reload data') //check news type key
-               // console.log(event)
-               wx.showToast({
-                  title: '努力加载中',
-                  icon: 'loading',
-                  duration: 1200
-               })
-      } 
-   },
-  switchNewsDetail(event) {
-     wx.navigateTo({
-        url: '/pages/news/news',
-     })
-      wx.showToast({
-         title: '努力加载中',
-         icon: 'loading',
-         duration: 1200
-      })
-  }
+    //激活当前新闻类别，更新获取新闻数据，显示loading
+    switchNewsType(event) {
+        const newType = event.currentTarget.dataset.nltype
+        // console.log(newType + ' new news type after click') //check news type key
+        if (newType !== this.data.activeType) {
+            this.setData({
+                activeType: newType
+            })
+            this.getNews()
+            // console.log(newType + ' after reload data') //check news type key
+            // console.log(event)
+            wx.showLoading({
+                title: '努力加载中',
+            })
+        }
+    }
+    /* change switch event to navigator
+    switchNewsDetail(event) {
+        wx.navigateTo({
+            url: '/pages/news/news',
+        })
+        wx.showToast({
+            title: '努力加载中',
+            icon: 'loading',
+            duration: 1200
+        })
+    }*/
 })
 
--- WX News Miniprogram/pages/news/news.js
+++ WX News Miniprogram/pages/news/news.js
@@ -2,93 +2,88 @@
 const util = require('../../utils/util.js')
 
 Page({
-  data: {
-     id0: 1523074607642,
-     newsDetail: [], //保存新闻详情数据
-     newsContent: []
-  },
+    data: {
+        id0: 1523074607642,
+        newsDetail: [], //保存新闻详情数据
+        newsContent: []
+    },
 
-onLoad: function (options) {
-      wx.showToast({
-         title: '努力加载中',
-         icon: 'loading',
-         duration: 1200
-      })
-      const id1 = options.id
-      this.setData ({
+    onLoad: function (options) {
+        const id1 = options.id
+        this.setData({
             id0: id1
-      })
-      //console.log(id1) check imported id and set data for id
-      this.getNews()
-  },
-  //获取API数据，更新数据
-  getNews(callback) {
-     const id2 = this.data.id0
-     //console.log(id2) check id for API input data
-     wx.request({
-        url: 'https://test-miniprogram.com/api/news/detail',
-        data: {id: id2},
-        //数据数组成功返回
-        success: res => {
-            let results = res.data.result
-            let contentLength = results.content.length //length of result data array
-            let code = res.data.code
-            //let hour = new Date().setTime()
-            let message = res.data.message
-            let newsDetail = []
-            let newsContent = []
-            if (code == "200" && message == "success") {
-                //  console.log(results, code, message, contentLength) 
-                  newsDetail.push({
-                     title: results.title,
-                     source:results.source,
-                     time: util.formatTime(new Date(results.date)), //转换时间格式
-                     count: '阅读 ' + results.readCount,
-                     image: !!results.firstImage ? results.firstImage : '/images/image-icon.png',
-                  }) 
-                  //get content type and info
-                  for (let i = 0; i < contentLength; i += 1) {
-                     newsContent.push({
-                        cid: results.content[i].id,
-                        ctype: results.content[i].type,
-                        ctext: results.content[i].text,
-                        csrc: results.content[i].src
-                     })
-                  }
-                  this.setData({
-                     newsDetail: newsDetail,
-                     newsContent: newsContent
-                  })
-                 // console.log(newsDetail + ' this is newsDetail')
-                //  console.log(newsContent[0].ctext  + ' this is newsContent')
-            } else {
-                     wx.showToast({
+        })
+        //console.log(id1) check imported id and set data for id
+        this.getNews()
+    },
+    //获取API数据，更新数据
+    getNews(callback) {
+        const id2 = this.data.id0
+        //console.log(id2) check id for API input data
+        wx.showLoading({
+            title: '努力加载中',
+        })
+        //显示加载
+        wx.request({
+            url: 'https://test-miniprogram.com/api/news/detail',
+            data: { id: id2 },
+            //数据数组成功返回
+            success: res => {
+                let results = res.data.result
+                let contentLength = results.content.length //length of result data array
+                let code = res.data.code
+                //let hour = new Date().setTime()
+                let message = res.data.message
+                let newsDetail = []
+                let newsContent = []
+                if (code == "200" && message == "success") {
+                    //  console.log(results, code, message, contentLength) 
+                    newsDetail.push({
+                        title: results.title,
+                        source: results.source,
+                        time: util.formatTime(new Date(results.date)), //转换时间格式
+                        count: '阅读 ' + results.readCount,
+                        image: !!results.firstImage ? results.firstImage : '/images/image-icon.png',
+                    })
+                    //get content type and info
+                    for (let i = 0; i < contentLength; i += 1) {
+                        newsContent.push({
+                            cid: results.content[i].id,
+                            ctype: results.content[i].type,
+                            ctext: results.content[i].text,
+                            csrc: results.content[i].src
+                        })
+                    }
+                    this.setData({
+                        newsDetail: newsDetail,
+                        newsContent: newsContent
+                    })
+                    // console.log(newsDetail + ' this is newsDetail')
+                    //  console.log(newsContent[0].ctext  + ' this is newsContent')
+                    wx.hideLoading({
+                        title: '加载中',
+                    })
+                } else {
+                    wx.showToast({
                         title: '加载错误, 请重试',
                         icon: 'none',
                         duration: 1800
-                     })
+                    })
+                }
+            },
+            fall: () => {
+                wx.showToast({
+                    title: '加载错误,请重试',
+                    icon: 'none',
+                    duration: 1800
+                })
+            },
+            complete: () => {
+                callback && callback()
             }
-        },
-       fall: () => {
-           wx.showToast({
-              title: '加载错误,请重试',
-              icon: 'none',
-              duration: 1800
-           })
-        },
-        complete: () => {
-           callback && callback()
-        }
-     })
-  },
-  returnToList(event) {
-     wx.navigateTo({
-        url: '/pages/index/index',
-     })
-     wx.showToast({
-        title: '努力加载中',
-        icon: 'loading',
-        duration: 1200
-     })
-  }
+        })
+    },
+    returnToList(event) {
+        wx.navigateBack()
+    }
 })
